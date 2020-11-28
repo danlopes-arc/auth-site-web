3d2a847ad2396aeff7b05e332ec96394709f61a7
Daniel <danlpsarcanjo@gmail.com>
2020-11-28 13:17:49 -0500

    Add conditional redering to NavBar
    NavBar now renders Login and Register buttons if user is not logged in,
    otherwise it renders their name and Logout button.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,53 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 import { Flex, Heading, Box, Button, useColorMode } from '@chakra-ui/react'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { AppDispatch, AppState } from '../store'
+import { logout } from '../store/system/actions'
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const loggedIn = useSelector((state: AppState) => state.system.loggedIn)
+  const user = useSelector((state: AppState) => state.system.user)
+  const dispatch = useDispatch<AppDispatch>()
+
   return (
     <Flex as="nav" align="center" justify="space-between" p={4} boxShadow="lg">
       <Link to="/">
         <Heading as="span">heyo</Heading>
       </Link>
       <Box>
-        <Button onClick={toggleColorMode}>
+        {loggedIn ? (
+          <Link to="/me">
+            <Heading as="span" fontSize="lg" ml={2}>
+              {user?.name}
+            </Heading>
+          </Link>
+        ) : null}
+
+        <Button ml={2} onClick={toggleColorMode}>
           {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
         </Button>
-        <Link to="/login">
-          <Button ml={2}>Login</Button>
-        </Link>
-        <Link to="/register">
-          <Button ml={2} colorScheme="purple">
-            Register
+
+        {loggedIn ? (
+          <Button ml={2} onClick={() => dispatch(logout())}>
+            Logout
           </Button>
-        </Link>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button ml={2}>Login</Button>
+            </Link>
+            <Link to="/register">
+              <Button ml={2} colorScheme="purple">
+                Register
+              </Button>
+            </Link>
+          </>
+        )}
       </Box>
     </Flex>
   )
